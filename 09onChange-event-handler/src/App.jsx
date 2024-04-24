b9265ca38a18ffea898a5f5cb15a55789936b6a1
Col-Ian <100207902+Col-Ian@users.noreply.github.com>
2024-04-24 13:33:29 -0230

    onChange event handler, Color picker app.

---
+++ 09onChange-event-handler/src/App.jsx
@@ -0,0 +1,18 @@
+import MyComponent from "./MyComponent.jsx"
+
+/*
+  onChange
+    event handler used primarliy with form elements
+    (<input>, <textarea>, <select>, <radio>, etc)
+    Triggers a function ever time the value of the input changes
+*/
+function App() {
+
+  return (
+    <>
+      <MyComponent />
+    </>
+  )
+}
+
+export default App
